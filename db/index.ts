6f136c8a90501e2ac0ea158a9c094741bab67e30
tk20211228 <t3kuboki@gmail.com>
2025-09-21 13:15:18 +0900

    feat(db): authSchemasを追加してデータベーススキーマを拡張

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,10 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
+import * as authSchemas from './schemas/auth'
 
 const client = postgres(process.env.DATABASE_URL!, { prepare: false })
-export const db = drizzle({ client });
+export const db = drizzle({ client ,
+  schema: {
+    ...authSchemas,
+  }
+});
